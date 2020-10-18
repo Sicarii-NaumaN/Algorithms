f81ea3d3775f2cc2f773bc6476cb875d988c8477
Naum Yashuvaev <hahalol915@gmail.com>
2020-10-18 16:42:52 +0300

    optimazer

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -2,49 +2,64 @@
 #include <cassert>
 #include <cmath>
 
-int binary(int end, int start, int *A, int &value) {
+
+int binary(int *A, int &value, int start, int end) {
     int middle;
+    int result = -1;
     middle = 0;
-    if (end - start == 1) {           // если массив состоит из 1 элемента, то просто
-        return middle;                // возвращаем 0-й индекс
-    }
     const int end_ = end;
-    while (true) {
+
+    while (true) {   // Начало цикла
         middle = (start + end) / 2;
-        if (value == A[middle]) {
-            return middle;
-        }
+
         if (start == end) {             // Если сошлись начальный и конечный индекс массивов,
             if (middle == 0) {          // то возвращаем текущий индекс - 1 (в случае 0-го текущего
-                return middle;          // индекса не отнимаем единицу)
+                result = middle;          // индекса не отнимаем единицу)
+                break;
             }
-            return middle - 1;
+            result = middle - 1;
+            break;
         }
-        if (middle > 0) {
-            if (A[middle] > value && A[middle - 1] < value) {
-                if (std::abs(value - A[middle - 1]) > std::abs(value - A[middle])) {
-                    return middle;
-                }
-                return (middle - 1);
-            }
-        }
-        if (middle < end_) {
-            if (A[middle] < value && A[middle + 1] > value) {
-                if (std::abs(value - A[middle]) > std::abs(value - A[middle + 1])) {
-                    return middle + 1;
-                }
-                return middle;
-            }
-        }
-
         if (A[middle] > value) {
             end = middle;
         }
         else {
             start = middle + 1;
         }
-    }
+    }                  // Конец цикла
+
+    if (result > 0) {
+            if (A[result] > value && A[result - 1] < value) {
+                if (std::abs(value - A[result - 1]) > std::abs(value - A[result])) {
+                    return result;
+                }
+                return (result - 1);
+            }
+        }
+        if (result < end_) {
+            if (A[result] < value && A[result + 1] > value) {
+                if (std::abs(value - A[result]) > std::abs(value - A[result + 1])) {
+                    return result + 1;
+                }
+                return result;
+            }
+        }
+    return result;
 }
+
+int exponential_search(int arr[], int size, int &key) {
+    if (size == 0) {
+        return EXIT_FAILURE;
+    }
+
+    int bound = 1;
+    while (bound < size && arr[bound] < key) {
+        bound *= 2;
+    }
+
+    return binary(arr, key, bound / 2, std::min(bound + 1, size));
+}
+
 void test() {
     int counter = 0;
 
@@ -52,7 +67,7 @@ void test() {
     int Tt[10] = {57, 7, 97, 80, 21, 10, 75, 46, 62, 52};
     int tt[10] = {5, 0, 9, 8, 0, 0, 8, 4, 6, 5};
     for (size_t i = 0; i < 10; ++i) {
-        counter = binary(10, 0, Zz, Tt[i]);
+        counter = exponential_search(Zz,10, Tt[i]);
         assert(counter == tt[i]);
     }
     std::cout << "OK\n";
@@ -60,7 +75,7 @@ void test() {
     int T[10] = {53, 7, 97, 80, 21, 10, 75, 46, 62, 52};
     int t[10] = {5, 0, 9, 8, 0, 0, 8, 4, 6, 5};
     for (size_t i = 0; i < 10; ++i) {
-        counter = binary(10, 0, Z, T[i]);
+        counter = exponential_search(Z,10, T[i]);
         assert(counter == t[i]);
     }
     std::cout << "OK\n";
@@ -68,7 +83,7 @@ void test() {
     int B[5] = {-2, 5, 4, 7, 12};
     int b[5] = {0, 2, 2, 2, 3};
     for (size_t i = 0; i < 5; ++i) {
-        counter = binary(4, 0, A, B[i]);
+        counter = exponential_search(A,4, B[i]);
         assert(counter == b[i]);
     }
 
@@ -78,7 +93,7 @@ void test() {
     int D[4] = {-3, -1, 0, 1};
     int d[4] = {0, 0, 0, 0};
     for (size_t i = 0; i < 4; ++i) {
-        counter = binary(4, 0, C, D[i]);
+        counter = exponential_search(C,4,  D[i]);
         assert(counter == d[i]);
     }
     std::cout << "OK\n";
@@ -87,7 +102,7 @@ void test() {
     int F[6] = {-3, 17, 3, 5, 0, 1};
     int f[6] = {0, 1, 0, 0, 0, 0};
     for (size_t i = 0; i < 6; ++i) {
-        counter = binary(2, 0, E, F[i]);
+        counter = exponential_search(E,2, F[i]);
         assert(counter == f[i]);
     }
     std::cout << "OK\n";
@@ -96,7 +111,7 @@ void test() {
     int J[2] = {10, -1};
     int j[2] = {2, 0};
     for (size_t i = 0; i < 2; ++i) {
-        counter = binary(5, 0, G, J[i]);
+        counter = exponential_search(G,5, J[i]);
         assert(counter == j[i]);
     }
     std::cout << "OK\n";
@@ -105,7 +120,7 @@ void test() {
     int P[7] = {-3, 0, 1, 2, 5, 7, 10};
     int p[7] = {0, 0, 0, 0, 2, 2, 2};
     for (size_t i = 0; i < 7; ++i) {
-        counter = binary(3, 0, H, P[i]);
+        counter = exponential_search( H,3,  P[i]);
         assert(counter == p[i]);
     }
     std::cout << "OK\n";
@@ -114,7 +129,7 @@ void test() {
     int Y[10] = {-13, -2, 0, 1, 245, 6, 8, 10, 11, 13};
     int y[10] = {0, 1, 2, 2, 10, 4, 5, 7, 8, 9};
     for (size_t i = 0; i < 10; ++i) {
-        counter = binary(11, 0, X, Y[i]);
+        counter = exponential_search( X,11,  Y[i]);
         assert(counter == y[i]);
     }
     std::cout << "OK\n";
@@ -123,7 +138,7 @@ void test() {
     int K[4] = {0, 1, 12, -100};
     int k[4] = {0, 0, 0, 0};
     for (size_t i = 0; i < 4; ++i) {
-        counter = binary(1, 0, O, K[i]);
+        counter = exponential_search( O,1,  K[i]);
         assert(counter == k[i]);
     }
     std::cout << "OK\n";
@@ -132,7 +147,7 @@ void test() {
     int N[7] = {-9, 1, -10, 6, 7, 10, -30};
     int n[7] = {1, 1, 0, 1, 1, 2, 0};
     for (size_t i = 0; i < 7; ++i) {
-        counter = binary(3, 0, L, N[i]);
+        counter = exponential_search(L,3, N[i]);
         assert(counter == n[i]);
     }
     std::cout << "OK\n";
@@ -141,7 +156,7 @@ void test() {
     int M[8] = {2, 3, 4, 5, 6, 7, 8, 12};
     int m[8] = {1, 2, 2, 2, 2, 2, 3, 4};
     for (size_t i = 0; i < 8; ++i) {
-        counter = binary(5, 0, V, M[i]);
+        counter = exponential_search(V,5 , M[i]);
         assert(counter == m[i]);
     }
     std::cout << "OK\n";
@@ -150,7 +165,7 @@ void test() {
     int S[7] = {-13000, 1, 1, 110000, -15, -15, -30000};
     int s[7] = {0, 1, 1, 4, 0, 0, 0};
     for (size_t i = 0; i < 7; ++i) {
-        counter = binary(5, 0, Q, S[i]);
+        counter = exponential_search(Q, 5, S[i]);
         assert(counter == s[i]);
     }
     std::cout << "OK\n";
@@ -170,9 +185,8 @@ int main() {
     for (size_t i = 0; i < m; i++) {
         std::cin >> B[i];
     }
-    size_t start = 0;
     for (size_t i = 0; i < m; i++) {
-        B[i] = binary(n, start, A, B[i]);
+        B[i] = exponential_search(A,n , B[i]);
     }
     for (size_t i = 0; i < m; i++) {
         std::cout << B[i] << " ";
