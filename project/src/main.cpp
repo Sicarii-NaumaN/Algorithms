0ae9a95d080a4d4199bc0cff0e480d613f33215b
Naum Yashuvaev <hahalol915@gmail.com>
2020-12-30 19:40:57 +0300

    all

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,54 +1,154 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 
-#include "ListGraph.h"
-#include "SetGraph.h"
-#include "ArcGraph.h"
-#include "MatrixGraph.h"
+struct IGraph {
+    virtual ~IGraph() {}
 
+    // Добавление ребра от from к to.
+    virtual void AddEdge(int from, int to) = 0;
+
+    virtual int VerticesCount() const  = 0;
+
+    virtual std::vector<int> GetNextVertices(int vertex) const = 0;
+    virtual std::vector<int> GetPrevVertices(int vertex) const = 0;
+};
+
+class ListGraph : public IGraph {
+public:
+    ListGraph(size_t verticesCount)  {
+        graph.resize(verticesCount);
+    }
+
+    ListGraph(const IGraph&);
+
+    virtual ~ListGraph() {}
+
+    // Добавление ребра от from к to.
+    void AddEdge(int from, int to) override {
+        graph[from].push_back(to);
+        graph[to].push_back(from);
+
+    }
+
+    int VerticesCount() const override { return graph.size(); }
+
+    std::vector<int> GetNextVertices(int vertex) const  override {
+        std::vector<int> result;
+        result.resize(graph[vertex].size());
+        std::copy(graph[vertex].begin(), graph[vertex].end(), result.begin());
+        return result;
+    }
+
+    std::vector<int> GetPrevVertices(int vertex) const override {
+        std::vector<int> result;
+        for (size_t i = 0; i < graph.size(); ++i) {
+            for (auto child : graph[i]) {
+                if (child == vertex) {
+                    result.push_back(i);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+private:
+    std::vector <std::vector<int> > graph;
+};
+
+void dfs_aux(const IGraph& graph, int vertex, std::vector<bool>& visited, void (*callback)(int v)) {
+    visited[vertex] = true;
+    callback(vertex);
+
+    for (auto child : graph.GetNextVertices(vertex)) {
+        if (!visited[child]) {
+            dfs_aux(graph, child, visited, callback);
+        }
+    }
+}
+
+void dfs(const IGraph& graph, void (*callback)(int v)) {
+    std::vector<bool> visited;
+    visited.resize(graph.VerticesCount(), false);
+
+    for (int i = 0; i < graph.VerticesCount(); ++i) {
+        if (!visited[i]) {
+            dfs_aux(graph, i, visited, callback);
+        }
+    }
+}
+
+void bfs(const IGraph& graph, void (*callback)(int v)) {
+    std::vector<bool> visited;
+    std::queue<int> queue;
+    visited.resize(graph.VerticesCount(), false);
+    for (int i = 0; i < graph.VerticesCount(); ++i) {
+        if (!visited[i]) {
+            queue.push(i);
+            visited[i] = true;
+            while (!queue.empty()) {
+                int vertex = queue.front();
+                queue.pop();
+                callback(vertex);
+                for (auto child : graph.GetNextVertices(vertex)) {
+                    if (!visited[child]) {
+                        queue.push(child);
+                        visited[child] = true;
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+int PathsCounter(const ListGraph &graph, int &a, int &b) {
+    int count = graph.VerticesCount();
+    int paths[count];
+    int depth[count];
+    std::queue<int> queue;
+
+    bool visited[count]; // INIT
+    for (size_t i = 0; i < count; ++i){
+        paths[i] = depth[i] = 0;
+        visited[i] = false;
+    }
+
+    queue.push(a);
+    paths[a] = 1;
+    while (!queue.empty()) {
+        int buf = queue.front();
+        queue.pop();
+
+        std::vector<int> vertices = graph.GetNextVertices(buf);
+        for (auto i : vertices) {
+            if (depth[i] == depth[buf] + 1) {
+                paths[i] += paths[buf];
+            } else if (!visited[i]) {
+                queue.push(i);
+                visited[i] = true;
+                depth[i] = depth[buf] + 1;
+                paths[i] = paths[buf];
+            }
+        }
+    }
+    return paths[b];
+}
 
 int main() {
-    ListGraph graph(6);
-    MatrixGraph graph_m(6);
-    SetGraph graph_s(6);
-    ArcGraph graph_a(6);
+    int v, n, a, b;
+    std::cin >> v >> n;
 
-    graph.AddEdge(0, 1);
-    graph.AddEdge(1, 2);
-    graph.AddEdge(1, 5);
-    graph.AddEdge(2, 3);
-    graph.AddEdge(3, 4);
-    graph.AddEdge(4, 2);
+    ListGraph graph(v);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a >> b;
+        graph.AddEdge(a, b);
+    }
 
-    graph_m.AddEdge(0, 1);
-    graph_m.AddEdge(1, 2);
-    graph_m.AddEdge(1, 5);
-    graph_m.AddEdge(2, 3);
-    graph_m.AddEdge(3, 4);
-    graph_m.AddEdge(4, 2);
-
-    graph_a.AddEdge(0, 1);
-    graph_a.AddEdge(1, 2);
-    graph_a.AddEdge(1, 5);
-    graph_a.AddEdge(2, 3);
-    graph_a.AddEdge(3, 4);
-    graph_a.AddEdge(4, 2);
-
-    graph_s.AddEdge(0, 1);
-    graph_s.AddEdge(1, 2);
-    graph_s.AddEdge(1, 5);
-    graph_s.AddEdge(2, 3);
-    graph_s.AddEdge(3, 4);
-    graph_s.AddEdge(4, 2);
-
-    dfs(graph, [](int v) {
-        std::cout << v << std::endl;
-    });
-    std::cout << "------------------------" << std::endl;
-    bfs(graph, [](int v) {
-        std::cout << v << std::endl;
-    });
-
-
+    std::cin >> a >> b;
+    auto res = PathsCounter(graph, a, b);
+    std::cout << res;
     return 0;
 }
